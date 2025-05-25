f339eba329ff141618707d299b32b89f30296673
HiroPlay22 <info@hiroplay.de>
2025-05-25 22:09:54 +0200

    sdsds

--- run-dev.ts
+++ run-dev.ts
@@ -1,9 +1,10 @@
+// run-dev.ts
 import "dotenv/config";
 import "tsconfig-paths/register";
 
 console.log("🟢 Starte M.E.A.T. (Bot + Webserver)...");
 
-// Helper für __dirname in ESM
+// Helper für file://
 import { fileURLToPath } from "url";
 import path from "path";
 const __filename = fileURLToPath(import.meta.url);
@@ -11,13 +12,13 @@ const __dirname = path.dirname(__filename);
 
 async function start() {
   try {
-    // ✅ 1. Bot starten
-    const botPath = path.resolve(__dirname, "bot/index.ts");
+    // 1. Bot starten
+    const botPath = path.resolve(__dirname, "./bot/index.ts");
     await import(`file://${botPath}`);
     console.log("✅ Bot-Modul geladen.");
 
-    // ✅ 2. Webserver starten
-    const serverPath = path.resolve(__dirname, "server.ts");
+    // 2. Webserver starten
+    const serverPath = path.resolve(__dirname, "./server.ts");
     await import(`file://${serverPath}`);
     console.log("✅ Webserver-Modul geladen.");
   } catch (err) {
