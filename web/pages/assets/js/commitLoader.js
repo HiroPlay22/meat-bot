1d1937d1e10d3eb39b7aa2af8e48df5f10e19390
HiroPlay22 <info@hiroplay.de>
2025-05-25 18:32:10 +0200

    Uhrzeit entfernt

--- web/pages/assets/js/commitLoader.js
+++ web/pages/assets/js/commitLoader.js
@@ -12,8 +12,6 @@ document.addEventListener("DOMContentLoaded", async () => {
           day: "2-digit",
           month: "2-digit",
           year: "numeric",
-          hour: "2-digit",
-          minute: "2-digit",
         });
 
         return `
