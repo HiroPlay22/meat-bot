594ba841d5bda75004ab32ea21d4014c59299ed0
HiroPlay22 <info@hiroplay.de>
2025-05-26 11:39:35 +0200

    twitchTest.ts fixed

--- test/twitchTest.ts
+++ test/twitchTest.ts
@@ -2,10 +2,9 @@
 
 import { getLiveStreams } from '../bot/modules/live/getLiveStreams.js';
 
-
 (async () => {
   try {
-    const usernames = ['hiro_live', 'brinibrinsen']; // hier deine Twitch-User eintragen
+    const usernames = ['hiro_live', 'brinibrinsen', 'gamingfriends_', 'perrick']; // Tracked Twitch-User eintragen
     const streams = await getLiveStreams(usernames);
 
     if (streams.length === 0) {
@@ -13,7 +12,7 @@ import { getLiveStreams } from '../bot/modules/live/getLiveStreams.js';
     } else {
       for (const stream of streams) {
         console.log(`✅ ${stream.username} ist live mit "${stream.title}" (${stream.viewers} Zuschauer) – Spiel: ${stream.game}`);
-        console.log(`Thumbnail: ${stream.thumbnail}`);
+        console.log(`🖼️ Thumbnail: ${stream.thumbnail}`);
         console.log('---');
       }
     }
