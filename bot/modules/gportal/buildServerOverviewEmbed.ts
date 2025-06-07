17304cf63ba2cc1773d22737a6c8f9b22283512e
HiroPlay22 <info@hiroplay.de>
2025-06-07 18:02:30 +0200

    Banner Update

--- bot/modules/gportal/buildServerOverviewEmbed.ts
+++ bot/modules/gportal/buildServerOverviewEmbed.ts
@@ -7,6 +7,6 @@ export function buildServerOverviewEmbed(): EmbedBuilder {
       'Unsere aktiven Community-Server.',
       'Wähle unten einen Server aus, um mehr Infos zu erhalten.'
     ].join('\n'))
-    .setImage('https://your.image.url/here.webp') // <- dein GPORTAL-Banner
+    .setImage('https://media.discordapp.net/attachments/1374459199181951087/1379485079449112606/ChatGPT_Image_3._Juni_2025_17_20_29.png?ex=68450658&is=6843b4d8&hm=02b2016657f1a5ea65d82573da94dced8b94c73dc889d51530bbf896d0755303&=&format=webp&quality=lossless&width=1229&height=819')
     .setColor(0x00ff88);
 }
