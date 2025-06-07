f11693d3ac973c402d4632477b215dd73ab1522e
HiroPlay22 <info@hiroplay.de>
2025-06-07 19:59:11 +0200

    ff

--- bot/modules/gportal/buildServerInfoEmbed.ts
+++ bot/modules/gportal/buildServerInfoEmbed.ts
@@ -9,20 +9,20 @@ export function buildServerInfoEmbed(config: GportalServerConfig, live: LiveServ
     .setColor(0x00ff88)
     .setDescription(
       `${emoji.meat_online} Ping: ${live?.ping ?? '??'} ms\n` +
-      `${emoji.meat_game} Map: ${config.game}\n` +
+      `${emoji.meat_game} Game: ${config.game}\n` +
       `${emoji.meat_leer} Map: ${live?.map || 'unbekannt'}\n` +
       `${emoji.meat_roles} Zugriff: nur mit entsprechender Rolle`
     )
     .setImage(
-      'https://media.discordapp.net/attachments/1374459199181951087/1379485079449112606/ChatGPT_Image_3._Juni_2025_17_20_29.png?ex=68450658&is=6843b4d8&hm=02b2016657f1a5ea65d82573da94c73dc889d51530bbf896d0755303&=&format=webp&quality=lossless&width=1229&height=819'
+      'https://media.discordapp.net/attachments/1374459199181951087/1379485079449112606/ChatGPT_Image_3._Juni_2025_17_20_29.png?ex=68450658&is=6843b4d8&hm=02b2016657f1a5ea65d82573da94dced8b94c73dc889d51530bbf896d0755303&=&format=webp&quality=lossless&width=1229&height=819'
     );
 
   if (live) {
     const labels = [
-      `${emoji.meat_users} Spieler:`.padEnd(14),
-      `${emoji.meat_leer} CPU:`.padEnd(14),
-      `${emoji.meat_leer} RAM:`.padEnd(14),
-      `${emoji.meat_leer} Speicher:`.padEnd(14)
+      `${emoji.meat_users} Spieler:`,
+      `${emoji.meat_leer} CPU:`,
+      `${emoji.meat_leer} RAM:`,
+      `${emoji.meat_leer} Speicher:`
     ];
 
     const bars = [
@@ -32,13 +32,16 @@ export function buildServerInfoEmbed(config: GportalServerConfig, live: LiveServ
       `${getBarLineOnlyBar(60)}  60 %`
     ];
 
-    const lines = labels.map((label, i) => `${label}${bars[i]}`);
-
     embed.addFields([
       {
-        name: 'Serverauslastung',
-        value: lines.join('\n'),
-        inline: false
+        name: ' ',
+        value: labels.join('\n'),
+        inline: true
+      },
+      {
+        name: ' ',
+        value: bars.join('\n'),
+        inline: true
       }
     ]);
   } else {
